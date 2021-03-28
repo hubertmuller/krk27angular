d135ad397da069f13052420015c8d26bd8f6ccce
Hubert Muller <hubert@sportport.eu>
2021-03-28 11:20:24 +0200

    mock2

--- tools/mock-server.js
+++ tools/mock-server.js
@@ -3,13 +3,23 @@ const port = 3200;
 
 const server = http.createServer( (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
-  //:res.setHeader('Content-type', 'application/json');
+  //res.setHeader('Content-type', 'text/plain');
+  res.setHeader('Content-type', 'application/json');
   let test = Math.random();
-  res.end(`<html>
-    <head></head>
-    <body>Czesc ${test}</body>
-    </html>`);
+  res.end(`[
+    {
+    "id": 1,
+    "name": "Pfizer",
+    details: {
+      "price": 200, "country": "us"
+    },
+    {
+    "id": 2,
+    "name": "Novavax",
+    details: {
+      "price": 220, "country": "gb"
+    }
+  ]`);
 });
 
 server.listen(port, '127.0.0.1', () => {
