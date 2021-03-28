75f68a6e0dbc98591dae63f29243fd857f953e51
Hubert Muller <hubert@sportport.eu>
2021-03-28 14:52:18 +0200

    detale gotowe

--- src/app/detale/detale.component.ts
+++ src/app/detale/detale.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ListaService, Szczepionka} from "../lista.service";
 
 @Component({
   selector: 'app-detale',
@@ -8,14 +10,20 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class DetaleComponent implements OnInit, OnDestroy {
 
   public id: number;
+  public szczepionka: Szczepionka;
 
-  constructor() {
-    console.log('constructror');
-    this.id = 5;
+  constructor(private route: ActivatedRoute, private listaService: ListaService) {
+
   }
 
   ngOnInit(): void {
-    console.log('oninit');
+    this.route.params.subscribe( (params) => {
+        this.id = params.id;
+        this.listaService.load().subscribe( (szczepionki) => {
+          this.szczepionka = szczepionki.find( (szczepionka: Szczepionka) => szczepionka.id == this.id);
+        });
+      }
+    )
   }
 
   ngOnDestroy(): void {
